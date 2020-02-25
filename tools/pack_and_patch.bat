b3beeb0d6de153a934af4fd56187ce577583a45f
Kryštof Montilla <montilla@jablotron.cz>
2020-02-25 11:06:24 +0100

    added icon patcher

--- tools/pack_and_patch.bat
+++ tools/pack_and_patch.bat
@@ -5,3 +5,4 @@ cd tools
 DEL /Q /S packed
 warp-packer.exe --arch windows-x64 --input_dir ../bin/Release/netcoreapp3.1/publish --exec FindMyMACNotMacintosh.exe --output packed/FindMyMACNotMacintosh.exe
 amc.exe packed/FindMyMACNotMacintosh.exe 2
+ResourceHacker.exe -script changeIconScript.txt
