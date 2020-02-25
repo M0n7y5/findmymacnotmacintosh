e749a3517584a2da27f063ebd5b9400e3bcd32c8
Kryštof Montilla <montilla@jablotron.cz>
2020-02-25 16:38:50 +0100

    Fixed project paths

--- FindMyMACNotMacintosh/adonis-ui_OLD/scripts/NuGetPack.ps1
+++
@@ -1,3 +0,0 @@
-nuget pack ..\AdonisUI\AdonisUi.csproj -Build -Properties Configuration=Release
-nuget pack ..\AdonisUI.ClassicTheme\AdonisUI.ClassicTheme.csproj -Build -Properties Configuration=Release
-Read-Host -Prompt "Press Enter to exit"
